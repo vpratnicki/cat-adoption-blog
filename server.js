ad41e66faad10a8a954e76a35324411db0bd3279
vpratnicki <vanessa@pratnicki.net>
2022-11-01 22:11:31 -0400

    Cleaned up code

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-// const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({});
 
 // const routes = require('./controllers/');
@@ -20,8 +18,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(session(sess));
 
 // turn on routes
 // app.use(routes);
