23b44051c47d1404acb005429b77c112f30cf1f0
Svetlana Nesterova <sveta59n@yahoo.com>
2022-11-02 15:15:33 -0400

    create User model, CRUD routes fro User and login route to verify user's identity

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,30 @@
+// const router = require('express').Router();
+
+// // const apiRoutes = require('./api/');
+// const homeRoutes = require('./home-routes.js');
+// // const dashboardRoutes = require('./dashboard-routes.js');
+
+// router.use('/', homeRoutes);
+// // router.use('/dashboard', dashboardRoutes);
+// // router.use('/api', apiRoutes);
+
+// module.exports = router;
+
+
+
+
+
+// in order to test User routes I, Svetlana, commented out the portion above.
+
 const router = require('express').Router();
 
-// const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-// const dashboardRoutes = require('./dashboard-routes.js');
+const apiRoutes = require('./api');
 
-router.use('/', homeRoutes);
-// router.use('/dashboard', dashboardRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,26 +1,46 @@
-const path = require('path');
+// const path = require('path');
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+
+// const hbs = exphbs.create({});
+
+// // const routes = require('./controllers/');
+// const sequelize = require('./config/connection');
+
+// // set up the express app
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // set handlebars as the template engine
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // turn on routes
+// // app.use(routes);
+
+// // turn on connection to db and server
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+
+
 const express = require('express');
-const exphbs = require('express-handlebars');
-
-const hbs = exphbs.create({});
-
-// const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-// set up the express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// set handlebars as the template engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
