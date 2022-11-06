0026941f6a5371c6b9987f429d774d66f4ebff60
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-11-06 09:55:57 -0500

    middleware

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
 
 router.get('/', (req, res) => {
   res.render('dashboard', { loggedIn: true });
