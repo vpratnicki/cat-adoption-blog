aa901ac9e280813ae1b8299025fc1b7c96dc46ca
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-11-05 20:55:45 -0400

    dashboard template

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 
-// // const dashboardRoutes = require('./dashboard-routes.js');
-// // router.use('/dashboard', dashboardRoutes);
+const dashboardRoutes = require('./dashboard-routes.js');
+
 
 
 
@@ -12,6 +12,7 @@ const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
